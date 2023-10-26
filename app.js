a4dfcc8370d44e90199be603bbeb8faeb0e0891e
reaget <reaget.acc@gmail.com>
2023-10-26 15:55:51 +0300

    fix(oembed): changed html

--- app.js
+++ app.js
@@ -25,10 +25,7 @@ app.get("/oembed", (req, res) => {
     "provider_name": "Express",
     "type": "video",
     "title": "Bg video",
-    "html": `<div style='position:relative;padding-bottom: calc(56.25%)'>
-        <iframe src='https://embed-vid2.onrender.com/ifr' style='position:absolute;width:100%;margin: 0; padding: 0;top:0;left:0;' frameborder='0' scrolling='no' width='1280' height='720' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>
-      </div>
-    `,
+    "html": "<div style='position:relative;padding-bottom: calc(56.25%)'><iframe src='https://embed-vid2.onrender.com/ifr' style='position:absolute;width:100%;margin: 0; padding: 0;top:0;left:0;' frameborder='0' scrolling='no' width='1280' height='720' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe></div>",
     "width": 1280,
     "height": 720,
     "thumbnail_url": "https://embed-vid2.onrender.com/assets/poster.jpg",
