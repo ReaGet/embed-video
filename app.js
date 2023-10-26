1849ebd3f1d8a7fe4a32928a7bf7e7df22ed463a
reaget <reaget.acc@gmail.com>
2023-10-26 15:06:46 +0300

    feat: add wrapper for iframe

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ app.get("/oembed", (req, res) => {
     "provider_name": "Express",
     "type": "video",
     "title": "Bg video",
-    "html": "<iframe src='https://embed-vid2.onrender.com/ifr' style='margin: 0; padding: 0;' frameborder='0' scrolling='no' width='1280' height='720' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>",
+    "html": `<div style='position:relative;padding-bottom: calc(56.25%)'>
+        <iframe src='https://embed-vid2.onrender.com/ifr' style='position:absolute;width:100%;margin: 0; padding: 0;top:0;left:0;' frameborder='0' scrolling='no' width='1280' height='720' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>
+      </div>
+    `,
     "width": 1280,
     "height": 720,
     "thumbnail_url": "https://embed-vid2.onrender.com/assets/poster.jpg",
